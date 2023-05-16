42821f6a5512eafd8730923b76850515bce24a56
Xavi Guasch <xaviguasch@gmail.com>
2023-05-16 18:36:24 +0200

    added Search component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 
 import './App.css'
+import Search from './components/Search'
 
 function App() {
   return (
     <>
       <h1>Jottings App</h1>
+      <Search />
     </>
   )
 }
---
+++ src/components/Search.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import classes from './Search.module.css'
+
+const Search = () => {
+  return <div className={classes.Search}>Search</div>
+}
+
+export default Search
